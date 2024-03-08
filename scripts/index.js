58d092b12eea15b46d39c6a1c49b5ecb37d040fe
McKay Knell <mckay@devpipeline.com>
2024-03-08 13:31:09 -0800

    updated assignment

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,21 @@
-class Number {
+class NumClass {
   constructor(array) {
     this.array = array;
   }
-}
-
-class Numbers {
-  constructor() {
-    this.Numbers = [];
-  }
-}
-
-const arrayNumbers = [
-  new Number("1"),
-  new Number("2"),
-  new Number("3"),
-  new Number("4"),
-  new Number("5"),
-];
-
-function arrayRemoval(array) {
-  for (let i = array.length; i--; ) {
-    if (i % 2 === 0) {
-      const shiftArray = array.shift();
-      console.log(shiftArray);
-    } else if (i % 2 != 0) {
-      const popArray = array.pop();
-      console.log(popArray);
+  arrayRemoval() {
+    for (let i = this.array.length; i--; ) {
+      if (i % 2 === 0) {
+        const shiftArray = this.array.shift();
+        console.log(shiftArray);
+      } else if (i % 2 != 0) {
+        const popArray = this.array.pop();
+        console.log(popArray);
+      }
     }
   }
 }
 
-console.log(arrayRemoval(arrayNumbers));
+const arrayNumbers = [1, 2, 3, 4, 5];
+
+const firstArr = new NumClass(arrayNumbers);
+firstArr.arrayRemoval(arrayNumbers);
